3024ad6111f696652f1752bc690410bb6334c001
Dimanteo <Kolobae.DA@phystech.edu>
2020-05-02 03:25:54 +0300

    Initial commit

---
+++ make.bat
@@ -0,0 +1,1 @@
+g++  patcher.cpp  -I C:\Libraries\SFML-2.5.1\include -L C:\Libraries\SFML-2.5.1\lib -lsfml-graphics -lsfml-audio -lopenal32 -lflac -lvorbisenc -lvorbisfile -lvorbis -logg -lsfml-window -lsfml-system -o hack.exe
